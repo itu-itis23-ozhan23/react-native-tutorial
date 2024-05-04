43915d4737a4f10415625aa80131ad271c094e79
Elif Işıl Özhan <ozhan23@itu.edu.tr>
2024-05-05 00:56:15 +0300

    view-text

---
+++ App.js
@@ -0,0 +1,12 @@
+import { View, Text, Image } from "react-native";
+const logıImg = require()
+export default function App() {
+  return (
+    <View style= {{flex: 1, backgroundColor:"plum", padding:60}}>
+    <Text>
+      <Text style={{color: "white"}}>Hello </Text> World
+    </Text>
+    </View>
+  );
+}
+ 
